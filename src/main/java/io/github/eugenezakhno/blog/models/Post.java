be35a844018a3fabe2150145c7c01ae2b41bbeb4
EugeneZakhno <zohan1234567@gmail.com>
2020-05-02 21:33:22 +0300

    set maven-compiler-plugin java 10 version

--- src/main/java/io/github/eugenezakhno/blog/models/Post.java
+++ src/main/java/io/github/eugenezakhno/blog/models/Post.java
@@ -1,14 +1,11 @@
 package io.github.eugenezakhno.blog.models;
 
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
 public class Post {
 
@@ -24,11 +21,49 @@ public class Post {
     public Post() {
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnons() {
+        return anons;
+    }
+
+    public void setAnons(String anons) {
+        this.anons = anons;
+    }
+
+    public String getFull_text() {
+        return full_text;
+    }
+
+    public void setFull_text(String full_text) {
+        this.full_text = full_text;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
 
     public Post(String title, String anons, String full_text) {
         this.title = title;
         this.anons = anons;
         this.full_text = full_text;
     }
-
 }

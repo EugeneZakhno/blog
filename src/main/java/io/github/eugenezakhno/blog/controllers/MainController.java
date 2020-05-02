59c661746dfdb7545d67af041b4ef0387ca42411
EugeneZakhno <zohan1234567@gmail.com>
2020-05-03 00:34:43 +0300

    changed url for home page

--- src/main/java/io/github/eugenezakhno/blog/controllers/MainController.java
+++ src/main/java/io/github/eugenezakhno/blog/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Main page");
         return "home";
